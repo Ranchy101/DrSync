d8a36f1f6625a7ec96deb9601f98aa1656aa96ff
Ranchy101 <42690717+Ranchy101@users.noreply.github.com>
2022-11-17 08:46:57 -0800

    Multi-Sensor Framework Update

--- Logger.java
+++ Logger.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class Logger {
     public static void main(String[] args) throws InterruptedException, IOException, SQLException, ClassNotFoundException {
+    	//---------------------------------------------JDBC MySQL Setup-------------------------------------------------------------------------
     		Class.forName("com.mysql.cj.jdbc.Driver");
 	    	String url = "jdbc:mysql://database-1.cqwrgzkfrky6.us-west-2.rds.amazonaws.com/SyncData";
 			String user = "admin";
@@ -24,131 +25,139 @@ public class Logger {
     		System.out.println("Enter patient room number:");
     		String room = sc.nextLine();
     		//once finished
-    		sc.close();
-    		Runtime.getRuntime().exec("C:\\Users\\Daniel\\eclipse-workspace\\heartrate\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+.exe", null, new File("C:\\Users\\Daniel\\eclipse-workspace\\heartrate\\SimulANT+ 2.3.0\\SimulANT+\\"));
+    		String currDir = System.getProperty("user.dir");
+    		Runtime.getRuntime().exec(currDir+"\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+.exe", null, new File(currDir+"\\SimulANT+ 2.3.0\\SimulANT+\\"));
     		 //----------------------------------------------PRODUCT VERIFICATION-------------------------------------------------------------------------
-	    	 File events = new File("C:\\Users\\Daniel\\eclipse-workspace\\heartrate\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display Events.txt");
-	    	 if (events.delete()) {
-		         System.out.println("Deleting old logs...");
-		         System.out.println("Start logging now.");
-		         Thread.sleep(10000);
-		         System.out.println("Turn on the heart rate monitor");
-		         Thread.sleep(15000);
-		         String word = "53848";
-		         boolean flag = false;
-		         int count = 0;
-		         Scanner sc2 = new Scanner(new FileInputStream("C:\\Users\\Daniel\\eclipse-workspace\\heartrate\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display Events.txt"));
-		         while(sc2.hasNextLine()) {
-		            String line = sc2.nextLine();
-		            if(line.indexOf(word)!=-1) {
-		               flag = true;
-		               count = count+1;
+    		 while (true) {
+	    		 System.out.println("1. Heartrate\n2. Temperature\n3. Close");
+	    		 int choice = sc.nextInt();
+	    		 if (choice == 1) {
+			    	 File events = new File(currDir+"\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display Events.txt");
+			    	 if (events.delete()) {
+				         System.out.println("Deleting old logs...");
+				         System.out.println("Start logging now.");
+				         Thread.sleep(10000);
+				         System.out.println("Turn on the heart rate monitor");
+				         Thread.sleep(15000);
+				         String word = "53848";
+				         boolean flag = false;
+				         int count = 0;
+				         Scanner sc2 = new Scanner(new FileInputStream(currDir+"\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display Events.txt"));
+				         while(sc2.hasNextLine()) {
+				            String line = sc2.nextLine();
+				            if(line.indexOf(word)!=-1) {
+				               flag = true;
+				               count = count+1;
+				            }
+				         }
+				         if(flag) {
+				        	 System.out.println("Product ID Verification Successful");
+				            //Correct Product ID
+				            //do nothing
+				         } else {
+				            System.out.println("Product ID Verification Failure");
+				            //terminate program
+				         }
+				     }
+				     else {
+				    	 System.out.println("Failed to delete old logs. Logging must be stopped first on SimulANT+ first.");
+				    	 System.out.println("Terminating Program...");
+				    	 //return;
+				     }
+				    //------------------------------------------------------------------------------------------------------------------------------------------
+				    @SuppressWarnings("resource")
+		            int[] heartArray = new int[10];//declare avg array
+		            int index = 0;
+		            while(true) {
+		            long millis = System.currentTimeMillis();
+		            //code to run every 1000 ms (1 second)
+		            RandomAccessFile fileHandler = null;
+		            try {
+		                File file = new File(currDir+"\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display ANT Messages.txt");
+		                fileHandler = new RandomAccessFile( file, "r" );
+		                long fileLength = fileHandler.length() - 1;
+		                StringBuilder sb = new StringBuilder();
+		                for(long filePointer = fileLength; filePointer != -1; filePointer--){
+		                    fileHandler.seek( filePointer );
+		                    int readByte = fileHandler.readByte();
+		    
+		                    if( readByte == 0xA ) {
+		                        if( filePointer == fileLength ) {
+		                            continue;
+		                        }
+		                        break;   
+		                    } else if( readByte == 0xD ) {
+		                        if( filePointer == fileLength - 1 ) {
+		                            continue;
+		                        }
+		                        break;
+		                    }
+		                    sb.append( ( char ) readByte );
+		                }
+		                String lastLine = sb.reverse().toString(); //get last line
+		                if (lastLine.contains("fail") || lastLine.contains("search")) {
+		                    //do nothing
+		                }
+		                else if (lastLine.contains("closed")) {
+		                	System.out.println("Device shut off");
+		                }
+		                else {
+		                	String hexString = lastLine.substring(lastLine.length() - 3);
+		                    String hex = hexString.replaceAll("]","");    //get heartrate hex value
+		                    int heartrate = Integer.parseInt(hex,16);  //convert to decimal
+		                    System.out.println(heartrate); //print heartrate for testing
+		                    heartArray[index] = heartrate;//add to array
+		                    index++; //only goes up if not fail
+		                }
+		            //if array full, print avg
+		            if (index == 10) {
+		                //get average of array
+		                int total = 0;
+		                for(int i=0; i<heartArray.length; i++){
+		                    total = total + heartArray[i];
+		                }
+		                int avg = total / heartArray.length;
+		                System.out.println("Average: " + avg); //print avg
+		                String query = "insert into PatientData" +
+		    					"(lastName, firstName, roomNumber, heartrate, patientID) " +
+		    					"values ('"+lastName+"', '"+firstName+"', '"+room+"', "+avg+",'"+patientID+"' )";
+		    			Connection connection = DriverManager.getConnection(url, user, password);
+		    			Statement statement = connection.createStatement();
+		    			statement.executeUpdate(query);
+		    			statement.close();
+		    			connection.close();
+		    			System.out.println("Patient data sent successfully.");
+		                index = 0; //reset index (no need to wipe array as it will be overwritten)
+		                if (avg < 60 || avg > 100)
+		                	System.out.println("WARNING");
+		                break;
+		                
 		            }
-		         }
-		         if(flag) {
-		        	 System.out.println("Product ID Verification Successful");
-		            //Correct Product ID
-		            //do nothing
-		         } else {
-		            System.out.println("Product ID Verification Failure");
-		            //terminate program
-		         }
-		     }
-		     else {
-		    	 System.out.println("Failed to delete old logs. Logging must be stopped first on SimulANT+ first.");
-		    	 System.out.println("Terminating Program...");
-		    	 //return;
-		     }
-		    //------------------------------------------------------------------------------------------------------------------------------------------
-		    @SuppressWarnings("resource")
-			//PrintWriter writer = new PrintWriter("Patient Data.txt", "UTF-8"); // create printable txt file
-            int[] heartArray = new int[10];//declare avg array
-            int index = 0;
-            while(true) {
-            long millis = System.currentTimeMillis();
-            //code to run every 1000 ms (1 second)
-            RandomAccessFile fileHandler = null;
-            try {
-                File file = new File("C:\\Users\\Daniel\\eclipse-workspace\\heartrate\\SimulANT+ 2.3.0\\SimulANT+\\SimulANT+ Logs - logs\\Heart Rate Display ANT Messages.txt");
-                fileHandler = new RandomAccessFile( file, "r" );
-                long fileLength = fileHandler.length() - 1;
-                StringBuilder sb = new StringBuilder();
-    
-                for(long filePointer = fileLength; filePointer != -1; filePointer--){
-                    fileHandler.seek( filePointer );
-                    int readByte = fileHandler.readByte();
-    
-                    if( readByte == 0xA ) {
-                        if( filePointer == fileLength ) {
-                            continue;
-                        }
-                        break;
-                        
-                    } else if( readByte == 0xD ) {
-                        if( filePointer == fileLength - 1 ) {
-                            continue;
-                        }
-                        break;
-                    }
-    
-                    sb.append( ( char ) readByte );
-                }
-                String lastLine = sb.reverse().toString(); //get last line
-                if (lastLine.contains("fail") || lastLine.contains("search")) {
-                    //do nothing
-                }
-                else if (lastLine.contains("closed")) {
-                	System.out.println("Device shut off");
-                	//break;
-                }
-                else {
-                	String hexString = lastLine.substring(lastLine.length() - 3);
-                    String hex = hexString.replaceAll("]","");    //get heartrate hex value
-                    int heartrate = Integer.parseInt(hex,16);  //convert to decimal
-                    System.out.println(heartrate); //print heartrate
-                    heartArray[index] = heartrate;//add to array
-                    index++; //only goes up if not fail
-                }
-            //if array full, print avg
-            if (index == 10) {
-                //get average of array
-                int total = 0;
-                for(int i=0; i<heartArray.length; i++){
-                    total = total + heartArray[i];
-                }
-                int avg = total / heartArray.length;
-                System.out.println("Average: " + avg); //print avg
-                String query = "insert into PatientData" +
-    					"(lastName, firstName, roomNumber, heartrate, patientID) " +
-    					"values ('"+lastName+"', '"+firstName+"', '"+room+"', "+avg+",'"+patientID+"' )";
-    			Connection connection = DriverManager.getConnection(url, user, password);
-    			Statement statement = connection.createStatement();
-    			statement.executeUpdate(query);
-    			statement.close();
-    			connection.close();
-    			System.out.println("Patient data sent successfully.");
-                index = 0; //reset index (no need to wipe array as it will be overwritten)
-                if (avg < 60 || avg > 100)
-                	System.out.println("WARNING");
-                break;
-                
-            }
-
-            } catch( java.io.FileNotFoundException e ) {
-                e.printStackTrace();
-                //return null;
-            } catch( java.io.IOException e ) {
-                e.printStackTrace();
-                //return null;
-            } finally {
-                if (fileHandler != null )
-                    try {
-                        fileHandler.close();
-                    } catch (IOException e) {
-                        /* ignore */
-                    }
-            }
-            Thread.sleep(1000 - millis % 1000);
-            }//timer
+		
+		            } catch( java.io.FileNotFoundException e ) {
+		                e.printStackTrace();
+		                //return null;
+		            } catch( java.io.IOException e ) {
+		                e.printStackTrace();
+		                //return null;
+		            } finally {
+		                if (fileHandler != null )
+		                    try {
+		                        fileHandler.close();
+		                    } catch (IOException e) {
+		                        /* ignore */
+		                    }
+		            }
+		            Thread.sleep(1000 - millis % 1000);
+		            }//timer
+	    		}//end of choice 1
+	    		else if (choice == 2) {
+			    	System.out.println("Temperature code goes here.");
+			    }
+			    else {
+			    	sc.close();
+			    	return;
+		    	}
+    		 }// BIG WHILE TRUE
     }//main
 }
